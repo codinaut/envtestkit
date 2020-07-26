9ec21f047907904837ec2292bbae44d78d663e40
Daniel <codinaut@gmail.com>
2020-07-27 02:00:50 +0700

    docs: racey test

--- examples/env_test.rs
+++ examples/env_test.rs
@@ -17,10 +17,12 @@ mod test {
     use super::*;
     use envtestkit::lock::{lock_read, lock_test};
     use envtestkit::set_env;
+    use std::{thread, time};
 
     #[test]
     fn get_name_default() {
         let _lock = lock_read();
+        thread::sleep(time::Duration::from_millis(100));
         assert_eq!(get_name(), "default-value")
     }
 
@@ -28,6 +30,24 @@ mod test {
     fn get_name_overriden() {
         let _lock = lock_test();
         let _test = set_env(OsString::from("NAME"), "not-default-one");
+
+        thread::sleep(time::Duration::from_millis(200));
+        assert_eq!(get_name(), "not-default-one")
+    }
+
+    #[test]
+    #[ignore]
+    fn get_name_racey() {
+        thread::sleep(time::Duration::from_millis(100));
+        assert_eq!(get_name(), "default-value")
+    }
+
+    #[test]
+    #[ignore]
+    fn get_name_racey_inducer() {
+        let _test = set_env(OsString::from("NAME"), "not-default-one");
+
+        thread::sleep(time::Duration::from_millis(200));
         assert_eq!(get_name(), "not-default-one")
     }
 }
