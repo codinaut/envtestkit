9681f6826f3c0b0c4f83cc82a3ced8bf6795a139
Daniel <codinaut@gmail.com>
2020-07-26 16:52:09 +0700

    refactor: rename gen_pair -> gen_key_value

--- src/testkit.rs
+++ src/testkit.rs
@@ -9,14 +9,18 @@ pub struct EnvironmentTestGuard {
 pub fn set_env<V: AsRef<OsStr>>(key: OsString, value: V) -> EnvironmentTestGuard {
     let prev_value = env::var_os(&key);
     env::set_var(&key, value);
-    EnvironmentTestGuard { key, value: prev_value }
+
+    EnvironmentTestGuard {
+        key,
+        value: prev_value,
+    }
 }
 
 impl Drop for EnvironmentTestGuard {
     fn drop(&mut self) {
         match &self.value {
             Some(value) => env::set_var(&self.key, value),
-            None => env::remove_var(&self.key)
+            None => env::remove_var(&self.key),
         }
     }
 }
@@ -28,21 +32,18 @@ mod test {
     use fake::Fake;
     use serial_test::serial;
 
-    fn gen_key() -> OsString {
-        OsString::from(Word().fake::<String>())
-    }
     fn gen_value() -> OsString {
         OsString::from(Sentence(1..10).fake::<String>())
     }
 
-    fn gen_pair() -> (OsString, OsString) {
-        (gen_key(), gen_value())
+    fn gen_key_value() -> (OsString, OsString) {
+        (OsString::from(Word().fake::<String>()), gen_value())
     }
 
     #[test]
     #[serial]
     fn set_env_ok() {
-        let (key, value) = gen_pair();
+        let (key, value) = gen_key_value();
         let _e = set_env(key.clone(), &value);
         assert_eq!(env::var_os(key).unwrap(), value)
     }
@@ -50,7 +51,7 @@ mod test {
     #[test]
     #[serial]
     fn set_env_clean_up() {
-        let (key, value) = gen_pair();
+        let (key, value) = gen_key_value();
         {
             let _e = set_env(key.clone(), &value);
         }
@@ -60,7 +61,7 @@ mod test {
     #[test]
     #[serial]
     fn set_env_restore() {
-        let (key, value) = gen_pair();
+        let (key, value) = gen_key_value();
         env::set_var(&key, &value);
         {
             let _e = set_env(key.clone(), gen_value());
